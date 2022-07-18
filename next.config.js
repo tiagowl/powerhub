b3d32895c31ef2cd480d632069b4a9b0744dd117
tiagolandi19 <tiago.landi@ubistart.com>
2022-07-18 10:57:54 -0300

    configuração varivel de ambiente

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     env: {
-        NEXT_PUBLIC_SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indib2xpY3NjaGdyaHpiem9pZGprIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTU5MjU4MzAsImV4cCI6MTk3MTUwMTgzMH0.wFxJJGUyyrmvm1_QJaaG2VM0vAD_abhP64-QJzYiA1Y"
+        NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY
     },
   }
